a874b982d8b5b3a71cb061e4091e540cc8ee6fd1
Ogaga <iyaraogaga69@gmail.com>
2022-07-13 19:03:55 +0100

    Fix linters error

--- decode.rb
+++ decode.rb
@@ -1,20 +1,26 @@
 def decode_alpha(alpha)
-    morse_dict = {
-      'A' => '.-', 'B' => '-...', 'C' => '-.-.', 'D' => '-..', 'E' => '.',
-      'F' => '..-.', 'G' => '--.', 'H' => '....',
-      'I' => '..', 'J' => '.---', 'K' => '-.-', 'L' => '.-..',
-      'M' => '--', 'N' => '-.', 'O' => '---', 'P' => '.--.',
-      'Q' => '--.-', 'R' => '.-.', 'S' => '...', 'T' => '-',
-      'U' => '..-', 'V' => '...-', 'W' => '.--', 'X' => '-..-',
-      'Y' => '-.--', 'Z' => '--..'
-    }
-    morse_dict.key(alpha)
-  end
-
-  def decode_word (text)
-    array = text.split
-    wordarray = []
-    array.each {|letter| wordarray.push(decode_char letter)}
-    wordarray.join
+  morse_dict = {
+    'A' => '.-', 'B' => '-...', 'C' => '-.-.', 'D' => '-..', 'E' => '.',
+    'F' => '..-.', 'G' => '--.', 'H' => '....',
+    'I' => '..', 'J' => '.---', 'K' => '-.-', 'L' => '.-..',
+    'M' => '--', 'N' => '-.', 'O' => '---', 'P' => '.--.',
+    'Q' => '--.-', 'R' => '.-.', 'S' => '...', 'T' => '-',
+    'U' => '..-', 'V' => '...-', 'W' => '.--', 'X' => '-..-',
+    'Y' => '-.--', 'Z' => '--..'
+  }
+  morse_dict.key(alpha)
 end
 
+def decode_word(text)
+  array = text.split
+  wordarray = []
+  array.each { |letter| wordarray.push(decode_char(letter)) }
+  wordarray.join
+end
+
+def decode_sent(sent)
+  myarray = sent.split('   ')
+  sentarray = []
+  myarray.each { |word| sentarray.push(decode_word(word)) }
+  puts sentarray.join(' ')
+end
